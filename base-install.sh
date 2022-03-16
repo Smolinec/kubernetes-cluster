70a654aa477d4d6951627e426aa5e474c822f9cd
Smolinec <jirysmolak@gmail.com>
2022-03-16 23:56:48 +0100

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get install docker.io
+sudo apt-get install docker.io -y
 sudo systemctl enable docker
 sudo systemctl status docker
 sudo systemctl start docker
