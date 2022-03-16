97d4a2d4b264569214bbafdb5a6011b46a2a71e7
Smolinec <jirysmolak@gmail.com>
2022-03-16 23:57:53 +0100

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -3,10 +3,10 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt-get install docker.io -y
 sudo systemctl enable docker
-sudo systemctl status docker
+#sudo systemctl status docker
 sudo systemctl start docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" -y
-sudo apt-get install kubeadm kubelet kubectl
+sudo apt-get install kubeadm kubelet kubectl -y
 sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
