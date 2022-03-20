9b66c8496762f38d38d000ad4067472e502208d3
Smolinec <jirysmolak@gmail.com>
2022-03-20 18:52:36 +0100

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -10,3 +10,8 @@ sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" -
 sudo apt-get install kubeadm kubelet kubectl -y
 sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list -y
+sudo apt-get update
+sudo apt-get install helm -y
