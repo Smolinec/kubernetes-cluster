84e5a2506ffd0c574b242e22432ae36f18f6f96b
Smolinec <jirysmolak@gmail.com>
2022-03-20 19:03:34 +0100

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -12,6 +12,6 @@ sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
 curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list -y
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm -y
