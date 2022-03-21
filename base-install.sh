129446d20a2f9deb35072b9c86c7fcabf121f2d6
Smolinec <jirysmolak@gmail.com>
2022-03-21 11:28:35 +0100

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install
 sudo apt update
-sudo apt upgrade -y
-sudo apt-get install docker.io -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
+sudo DEBIAN_FRONTEND=noninteractive apt-get install docker.io -yq
 sudo systemctl enable docker
 #sudo systemctl status docker
 sudo systemctl start docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" -y
-sudo apt-get install kubeadm kubelet kubectl -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install kubeadm kubelet kubectl -yq
 sudo apt-mark hold kubeadm kubelet kubectl
 kubeadm version
 curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
-sudo apt-get install helm -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install helm -yq
